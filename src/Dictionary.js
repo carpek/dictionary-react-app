e36024f8ce691a5a6f2e344837135685ea82cb22
carpek <77640624+carpek@users.noreply.github.com>
2021-05-23 15:15:55 +0200

    updated because bugs

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,24 +5,20 @@ import Results from './Results';
 
 export default function Dictionary() {
 
-    let [keyword, setKeyword] = useState("");
+let [keyword, setKeyword] = useState("");
 let [results, setResults] = useState(null); 
 
 function handleResponse(response) {
-console.log(response.data[0]);
-console.log(response.data[0].meanings[0].definitions[0]);
 setResults(response.data[0]);
 }
 
 function search(event) {
-event.preventDefault();
-
+    event.preventDefault();
 let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
-    }
+}
 
 function handleKeywordSearch(event) {
-
 setKeyword(event.target.value);
     }
 
