5de92de5eb374e5aa8768f24be6f5f6465ecb84e
carpek <77640624+carpek@users.noreply.github.com>
2021-05-23 13:14:22 +0200

    added new logo and basic css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" /> 
       </header>
+
+      <footer>
+          This app was coded by Lina Pek and is open sourced on GitHub
+        </footer>
     </div>
   );
 }
