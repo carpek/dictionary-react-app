d4cf4418bb6bb403639ce32fadd001309693b9bb
carpek <77640624+carpek@users.noreply.github.com>
2021-05-23 14:09:46 +0200

    added axios and api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './Dictionary.css';
 
 export default function Dictionary() {
 
     let [keyword, setKeyword] = useState("");
 
+function handleResponse(response) {
 
+
+}
 
 function search(event) {
 event.preventDefault();
-alert(`Searching for ${keyword}`);
-
+alert(`Searching for ${keyword} `);
+let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+axios.get(apiUrl).then(handleResponse);
     }
 
-    function handleKeywordSearch(event) {
+function handleKeywordSearch(event) {
 
-        setKeyword(event.target.value);
+setKeyword(event.target.value);
     }
 
 return ( <div className="Dictionary">
