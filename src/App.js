70bc42c51740a5ff0d95b9c60f576a4cb2b2931f
carpek <77640624+carpek@users.noreply.github.com>
2021-05-23 13:47:01 +0200

    added core functions for search engine

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.png';
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" /> 
       </header>
-
-      <footer>
+<Dictionary />
+      <footer className="App-footer">
           This app was coded by Lina Pek and is open sourced on GitHub
         </footer>
+    </div>
     </div>
   );
 }
